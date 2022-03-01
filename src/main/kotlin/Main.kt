4a37a654f568b032d7c10c05a4ec74c0e3a7aef8
Sunghoon Yang <96470529+yshush@users.noreply.github.com>
2022-03-02 01:28:37 +0900

    실습 3-3. Product 객체 추가 및 ProductList 화면이 상품 출력 담당

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,5 @@
 import screen.ShoppingHome
 
-/*
-    step 1. main 함수 -> 클래스의 함수
-    step 2. 화면별 클래스 분리
-    step 3. 선택한 카테고리 상품 목록 표시
-    */
 fun main(args: Array<String>) {
     val shoppingHome = ShoppingHome()
     shoppingHome.start()
--- src/main/kotlin/screen/ShoppingCategory.kt
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -18,8 +18,8 @@ class ShoppingCategory {
             // TODO 1. 장바구니 이동
         } else {
             if(categories.contains(selectedCategory)){
-                // TODO 2. 카테고리 상품 목록 보여주기
-
+                val shoppingProductList = ShoppingProductList()
+                shoppingProductList.showProducts(selectedCategory)
             } else {
                 showErrorMessage(selectedCategory)
             }
